10597e08d6ccd0de37513663412d302e2a9db51f
Shu Min Yang Chang <shu.minyang@hotmail.com>
2017-03-12 00:26:12 -0300

    commit

---
+++ js/app.js
@@ -0,0 +1,160 @@
+'use strict';
+
+var map;
+var markers = [];
+var infoWind;
+var bounds;
+var streetViewService;
+var radius = 50;
+
+function initMap() {
+
+  var options = {
+    center: {
+      lat: -23.564005,
+      lng: -46.627078
+    },
+    zoom: 14,
+    disableDefaultUI: true,
+    mapTypeControl: false,
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_BOTTOM,
+      style: google.maps.ZoomControlStyle.DEFAULT
+    },
+    panControlOptions: {
+      position: google.maps.ControlPosition.LEFT_BOTTOM
+    }
+  };
+  var locations = [{
+      title: "Museum of Art of São Paulo Assis Chateaubriand",
+      lat: -23.561518,
+      lng: -46.656009,
+      streetAddress: "Av. Paulista, 1578, São Paulo",
+      info: "The São Paulo Museum of Art (Portuguese: Museu de Arte de São Paulo, or MASP) " +
+      "is an art museum located on Paulista Avenue in the city of São Paulo, Brazil. It is well known for its headquarters, " +
+      "a 1968 concrete and glass structure designed by Lina Bo Bardi, whose main body is supported by two lateral beams over a " +
+      "74 metres (243 ft) freestanding space, considered a landmark of the city and a main symbol of modern Brazilian architecture."
+    }, {
+      title: "Aquário de São Paulo",
+      lat: -23.593378,
+      lng: -46.613979,
+      streetAddress: "R. Huet Bacelar, 407, São Paulo",
+      info: "Considered a reference in the treatment and exhibition of animals, " +
+      "São Paulo Aquarium is the largest aquarium in Latin America. It was opened in 2006, featuring 9 thousand m² and 2 million gallons of water." +
+      "The only thematic aquarium in Brazil, this complex induces visitors to feel immersed in the environments,  " +
+      "which have approximately 3000 specimens of 300 species of animals. Brazilian forests are portrayed in the first sector, " +
+      " dedicated to fresh water, and which addresses issues such as pollution of rivers and endangered species."
+    }, {
+      title: "Paulista Museum",
+      lat: -23.585578,
+      lng: -46.609679,
+      streetAddress: "Parque da Independência, São Paulo",
+      info: "The Museu Paulista of the University of São Paulo (commonly known in São Paulo and all Brazil as Museu do Ipiranga) " +
+      "is a Brazilian history museum located near where Emperor Pedro I proclaimed the Brazilian independence on the banks of Ipiranga " +
+      "brook in the Southeast region of the city of São Paulo, then the 'Caminho do Mar,' or road to the seashore. " +
+      "It contains a huge collection of furniture, documents and historically relevant artwork, especially relating to the Brazilian Empire era."
+    }, {
+      title: "Municipal Theatre of São Paulo",
+      lat: -23.545235,
+      lng: -46.638615,
+      streetAddress: "Praça Ramos de Azevedo, São Paulo",
+      info: "Municipal Theatre of São Paulo is a theatre in São Paulo, Brazil. It is regarded as one of the landmarks of the city, " +
+      "significant both for its architectural value as well as for its historical importance, having been the venue for the " +
+      "Week of Modern Art in 1922, which revolutionised the arts in Brazil. The building now houses the São Paulo Municipal Symphonic Orchestra, " +
+      "the Coral Lírico (Lyric Choir) and the City Ballet of São Paulo."
+    }, {
+      title: "Pinacoteca do Estado de São Paulo",
+      lat: -23.534267,
+      lng: -46.63395,
+      streetAddress: "Praça da Luz, São Paulo",
+      info: "The Pinacoteca do Estado de São Paulo is one of the most important art museums in Brazil. It is housed in a 1900 building in " +
+      "Jardim da Luz, Downtown São Paulo, designed by Ramos de Azevedo and Domiziano Rossi to be the headquarters of the Lyceum of Arts and Crafts. " +
+      "It is the oldest art museum in São Paulo, founded on December 24, 1905, and established as a state museum since 1911."
+    }, {
+      title: "Ibirapuera Park",
+      lat: -23.584843,
+      lng: -46.655913,
+      streetAddress: "Parque Ibirapuera, São Paulo",
+      info: "Ibirapuera Park is a major urban park in São Paulo, Brazil. It has a large area for leisure, jogging and walking, " +
+      "as well a vivid cultural scene with museums and a music hall. Its importance to São Paulo is often comparable to that of " +
+      "Central Park to New York City, Golden Gate Park to San Francisco, or Ueno Park to Tokyo. Ibirapuera is one of Latin America's " +
+      "largest city parks, together with Chapultepec Park in Mexico City and Simón Bolívar Park in Bogota."
+  }];
+  var element = $('#map-canvas');
+
+  infoWind = new google.maps.InfoWindow();
+  bounds = new google.maps.LatLngBounds();
+  streetViewService = new google.maps.StreetViewService();
+
+  map = new google.maps.Map(element[0], options);
+  initMarker(locations);
+}
+
+function initMarker(locations) {
+  for (var i = 0; i < locations.length; i++) {
+    var title = locations[i].title;
+    var coordinates = {
+      lat: locations[i].lat,
+      lng: locations[i].lng
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      position: coordinates,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      info: locations[i].info,
+      id: i
+    });
+
+    marker.addListener('click', function() {
+      createInfoWindow(this, infoWind);
+    });
+    markers.push(marker);
+    bounds.extend(marker.position);
+
+  }
+
+}
+
+function createInfoWindow(marker, infoWindow) {
+
+  function getStreetView(data, status) {
+    if (status == google.maps.StreetViewStatus.OK) {
+
+      var nearStreetViewLocation = data.location.latLng;
+
+      var heading = google.maps.geometry.spherical.computeHeading(
+        nearStreetViewLocation, marker.position);
+      var panoramaOptions = {
+        position: nearStreetViewLocation,
+        pov: {
+          heading: heading,
+          pitch: 30
+        }
+      };
+
+      var panorama = new google.maps.StreetViewPanorama($('#pano')[0], panoramaOptions);
+      console.log(panorama);
+    }
+  }
+
+  if (infoWindow.marker !== marker) {
+
+    infoWindow.marker = marker;
+    infoWindow.setContent('<div>' + marker.info + '</div><div id="pano"></div>');
+    infoWindow.open(map, marker);
+
+    infoWindow.addListener('closeClick', function() {
+      infoWindow.setMarker(null);
+    });
+
+    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+
+  }
+}
+
+var viewModel = {
+  query: ko.observable('');
+};
--- js/googleApp.js
+++
@@ -1,8 +0,0 @@
-(function(window, mapster, google) {
-    var options = mapster.MAP_OPTIONS;    
-    element = $('#map-canvas');
-
-    map = mapster.create(element[0], options);
-    map.createMarker();
-
-}(window, window.Mapster, google));
